7cad55531535edebe25d0b3396fd46ea7b86b48a
Arian Tershnjaku <arian.t@gjirafa.com>
2023-05-25 17:54:26 +0200

    add the price styling thing

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,52 @@
-"use client";
-
 import RestaurantCard from "./components/RestaurantCard";
 import Header from "./components/Header";
+import { PrismaClient, Cuisine, Location, PRICE } from "@prisma/client";
+
+export interface Restaurant {
+  id: number;
+  name: string;
+  description: string;
+  main_image: string;
+  slug: string;
+  cuisine: Cuisine;
+  location: Location;
+  price: PRICE;
+}
+
+const prisma = new PrismaClient();
+
+const fetchRestaurants = async (): Promise<Restaurant[]> => {
+  const restaurants = await prisma.restaurant.findMany({
+    select: {
+      id: true,
+      name: true,
+      description: true,
+      main_image: true,
+      slug: true,
+      cuisine: true,
+      location: true,
+      price: true,
+    },
+  });
+
+  return restaurants;
+};
 
 interface HomePageProps {}
 
-const HomePage: React.FC<HomePageProps> = () => {
+async function HomePage() {
+  const restaurants = await fetchRestaurants();
+
   return (
     <>
       <Header />
-      <div className="py-3 px-36 mt-10 flex flex-wrap justify-start">
-        <RestaurantCard />
+      <div className="py-3 px-36 mt-10 flex flex-wrap justify-center">
+        {restaurants.map((restaurant) => (
+          <RestaurantCard restaurant={restaurant} />
+        ))}
       </div>
     </>
   );
-};
+}
 
 export default HomePage;
